009ed2e81f1418e67a1521fde28df7fec8c2af98
Brandon Graham <bgraham452@gmail.com>
2018-08-01 14:49:11 -0400

    pushing for test

--- src/form.js
+++ src/form.js
@@ -3,14 +3,14 @@ import './form.css';
 import {reduxForm, Field } from 'redux-form';
 import { required, notEmpty, lengthOfFive, isNumber } from './validators';
 import Input from './input'
+import {complaint} from './server';
 
 export class ComplaintForm extends React.Component {
   render() {
     return (
       <form 
-      onSubmit={this.props.handleSubmit(values => 
-        console.log(values)
-      )}>
+      onSubmit={values => this.props.handleSubmit(complaint(values))
+      }>
       <div className="formContainer">
         <h2>Report a problem with your delivery</h2>
 
@@ -24,18 +24,22 @@ export class ComplaintForm extends React.Component {
           validate={[required, notEmpty, lengthOfFive, isNumber]} />
 
         <label htmlFor="issue">What is your issue?</label>
-        <Field component="select" name="input" id="Input">
-          <option value="notArrived">My delivery hasn't arrived</option>
-          <option value="wrongItem">The wrong item was delivered</option>
-          <option value="missing">Part of my order was missing</option>
+        <Field 
+        component={Input}
+        element="select" name="issue" 
+        id="Issue">
+          <option value="not-delivered">My delivery hasn't arrived</option>
+          <option value="wrong-item">The wrong item was delivered</option>
+          <option value="missing-part">Part of my order was missing</option>
           <option value="damaged">Some of my order arrived damanged</option>
           <option value="other">Other (give details below)</option>
         </Field>
 
         <label htmlFor="details">Give more details (optional)</label>
-        <Field component="textarea" type="input" name="details" id="Details" />
+        <Field component={Input}
+        element="textarea" type="input" name="details" id="Details" />
 
-        <button>Submit</button>
+        <button type="submit">Submit</button>
 
       </div>
       </form>
---
+++ src/server.js
@@ -0,0 +1,43 @@
+import {SubmissionError} from 'redux-form';
+
+
+export const complaint = values => dispatch => {
+    return fetch ('https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
+        method: 'POST',
+        body: JSON.stringify(values),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    })
+    .then(res => {
+       if (!res.ok){
+            if (
+                res.headers.has('content-type') &&
+                res.headers
+                    .get('content-type')
+                    .startsWith('application/json')
+            ){
+                return res.json().then(err => Promise.reject(err));
+            }
+            return Promise.reject({
+                code: res.status,
+                message: res.statusText
+            });
+        }
+        return;
+    })
+    .then(() => console.log('Submitted with values', values))
+    .catch(error => 
+        Promise.reject(
+            new SubmissionError({
+                [error.location]: error.message
+            })
+        )
+    );
+};
+
+
+
+
+
